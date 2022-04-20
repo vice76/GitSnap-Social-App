ec64dcd9e9cc5e25025a6367f9b925da6f3b5ffe
vice76 <arpityadavaj1999@gmail.com>
2022-04-20 19:58:31 +0530

    changed folder structure and installed react-router-dom

---
+++ api/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+
+dotenv.config();
+
+mongoose.connect(
+  process.env.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("Connected to MongoDb");
+  }
+);
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to homepage");
+});
+
+app.listen(8800, () => {
+  console.log("backend server is running");
+});

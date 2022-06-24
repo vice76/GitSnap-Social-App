197e4a8cd1a683031b71609f95630d39cc50f1e4
vice76 <arpityadavaj1999@gmail.com>
2022-06-24 16:53:46 +0530

    cancel button functionality feature added

--- client/src/components/share/Share.jsx
+++ client/src/components/share/Share.jsx
@@ -3,6 +3,7 @@ import PermMediaIcon from "@mui/icons-material/PermMedia";
 import LabelIcon from "@mui/icons-material/Label";
 import RoomIcon from "@mui/icons-material/Room";
 import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
+import CancelIcon from "@mui/icons-material/Cancel";
 import { AuthContext } from "../../context/AuthContext";
 import axios from "axios";
 import "./share.css";
@@ -56,6 +57,15 @@ export default function Share() {
           />
         </div>
         <hr className="shareHr" />
+        {file && (
+          <div className="shareImgContainer">
+            <img className="shareImg" src={URL.createObjectURL(file)} alt="" />
+            <CancelIcon
+              className="shareCancelImg"
+              onClick={() => setFile(null)}
+            />
+          </div>
+        )}
         <form className="shareBottom" onSubmit={submitHandler}>
           <div className="shareOptions">
             <label htmlFor="file" className="shareOption">

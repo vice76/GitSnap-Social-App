8b9a245e96249426080440462197f480589c5beb
vice76 <arpityadavaj1999@gmail.com>
2022-06-25 20:55:24 +0530

    fixed bugs

--- client/src/pages/messenger/Messenger.jsx
+++ client/src/pages/messenger/Messenger.jsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useEffect, useContext, useState } from "react";
 import ChatOnline from "../../components/chatOnline/ChatOnline";
 import Conversation from "../../components/conversations/Conversation";
 import Message from "../../components/message/Message";
 import Topbar from "../../components/topbar/Topbar";
+import { AuthContext } from "../../context/AuthContext";
+import axios from "axios";
 import "./messenger.css";
 
 export default function Messenger() {
+  const [conversations, setConversations] = useState([]);
+  const { user } = useContext(AuthContext);
+
+  //get conersation
+  useEffect(() => {
+    const getConversations = async () => {
+      try {
+        const res = await axios.get("/conversations/" + user._id);
+        setConversations(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getConversations();
+  }, [user._id]);
+
+  //get messages
+  useEffect(() => {
+    const getMessages = async () => {
+      try {
+        const res = await axios.get("/messages/" + currentChat?._id);
+        setMessages(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getMessages();
+  }, [currentChat]);
+
   return (
     <>
       <Topbar />
@@ -13,7 +44,11 @@ export default function Messenger() {
         <div className="chatMenu">
           <div className="chatMenuWrapper">
             <input placeholder="Search for friends" className="chatMenuInput" />
-            <Conversation />
+            {conversations.map((c) => (
+              <div onClick={() => setCurrentChat(c)}>
+                <Conversation conversation={c} currentUser={user} />
+              </div>
+            ))}
           </div>
         </div>
         <div className="chatBox">

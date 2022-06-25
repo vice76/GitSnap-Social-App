fa52f534b190d150dc00287ba2adce13e408d8b7
vice76 <arpityadavaj1999@gmail.com>
2022-06-25 15:36:12 +0530

    implemented add conversation api

--- api/routes/messages.js
+++ api/routes/messages.js
@@ -1,4 +1,17 @@
 const router = require("express").Router();
 const Message = require("../models/Message");
 
+//add
+
+router.post("/", async (req, res) => {
+  const newMessage = new Message(req.body);
+
+  try {
+    const savedMessage = await newMessage.save();
+    res.status(200).json(savedMessage);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;

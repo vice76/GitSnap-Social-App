a83a1fd3e9221c0eed95eb33020783cc94f76e8d
vice76 <arpityadavaj1999@gmail.com>
2022-06-25 15:41:11 +0530

    implemented get conversation api

--- api/routes/messages.js
+++ api/routes/messages.js
@@ -14,4 +14,16 @@ router.post("/", async (req, res) => {
   }
 });
 
+//get
+router.get("/:conversationId", async (req, res) => {
+  try {
+    const messages = await Message.find({
+      conversationId: req.params.conversationId,
+    });
+    res.status(200).json(messages);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
